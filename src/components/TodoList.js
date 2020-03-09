8b3cc21e89a26f6306a302f5d1a6887a07ced702
Reagan Barrington <reagan@barringtonfamily.net>
2020-03-09 17:24:24 -0400

    centering button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,14 +4,16 @@ import "./Todo.css";
 
 const ToDoList = props => {
     return (
-        <div className="todo-list">
-            {props.todos.map(item => (
-                <Todo 
-                    key={item.id} 
-                    item={item} 
-                    toggleItem={props.toggleItem} 
-                />
-            ))}
+        <div className="todo-list-container">
+            <div className="todo-list">
+                {props.todos.map(item => (
+                    <Todo 
+                        key={item.id} 
+                        item={item} 
+                        toggleItem={props.toggleItem} 
+                    />
+                ))}
+            </div>
             <button className='clear-btn' onClick={props.clearCompleted}>Clear Completed Items</button>
         </div>
     )
